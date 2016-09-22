ac4843d2c1429cf3fdc17cb487273609f2f163f9
Eric Hainer <eric.hainer@gmail.com>
2016-09-22 10:45:37 -0600

    Adds install generator

---
+++ lib/generators/voltron/install_generator.rb
@@ -0,0 +1,15 @@
+module Voltron
+	module Generators
+		class InstallGenerator < Rails::Generators::Base
+
+			source_root File.expand_path("../../../../../spec/railsapp/config/initializers", __FILE__)
+
+			desc "Add Voltron initializer"
+
+			def copy_initializer
+				copy_file "voltron.rb", Rails.root.join("config", "initializers", "voltron.rb")
+			end
+
+		end
+	end
+end
--- spec/railsapp/config/initializers/voltron.rb
+++ spec/railsapp/config/initializers/voltron.rb
@@ -1,3 +1,9 @@
 Voltron.setup do |config|
+
+	# Whether to enable debug output in the browser console and terminal window
 	config.debug = true
+
+	# The base url of the site. Used by various voltron-* gems to correctly build urls
+	config.base_url = "http://localhost:3000"
+
 end
