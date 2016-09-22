2aaec302266ea2bb102855e2242929cee07d6ac8
Eric Hainer <eric.hainer@gmail.com>
2016-09-22 17:01:56 -0600

    Fixes source_root path, typo

--- lib/generators/voltron/install_generator.rb
+++ lib/generators/voltron/install_generator.rb
@@ -2,7 +2,7 @@ module Voltron
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      source_root File.expand_path("../../config/initializers", __FILE__)
+      source_root File.expand_path("../../templates/config/initializers", __FILE__)
 
       desc "Add Voltron initializer"
 
