719bc7638922931a1328b1d3d040e4a594797da6
Eric Hainer <eric.hainer@gmail.com>
2016-09-22 10:48:41 -0600

    Updates source_root

--- lib/generators/voltron/install_generator.rb
+++ lib/generators/voltron/install_generator.rb
@@ -2,7 +2,7 @@ module Voltron
 	module Generators
 		class InstallGenerator < Rails::Generators::Base
 
-			source_root File.expand_path("../../../../../spec/railsapp/config/initializers", __FILE__)
+			source_root File.expand_path("../../../../spec/railsapp/config/initializers", __FILE__)
 
 			desc "Add Voltron initializer"
 
