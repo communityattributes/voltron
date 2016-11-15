71203c26e4447229c06a08b5ead633d096022571
Eric Hainer <eric.hainer@gmail.com>
2016-11-15 12:38:04 -0700

    Refactors Voltron core, behaves as both function and object

--- app/assets/javascripts/voltron-core.js
+++ app/assets/javascripts/voltron-core.js
@@ -1,166 +1,193 @@
-var Voltron = (function($){
-  var config = {};
-  var observer = {};
-  var modules = {};
-  var classes = {};
+window.Voltron = (function(cmd){
+  var command = cmd.split('/', 2);
+  var module = command[0];
+  var method = command[1] || 'initialize';
 
-  return {
-    initialize: function(conf){
-      if(!conf) conf = {};
-      $.extend(config, conf);
-
-      // Try and create a module with the name of the current controller
-      if(this.hasModule(this.getConfig('controller'))){
-        this.ready(Voltron.getModule, this.getConfig('controller'));
-      }
-    },
-
-    // When ready, fire the callback function, passing in any additional args
-    ready: function(callback, args){
-      $(document).ready(function(){
-        if(!$.isArray(args)) args = [args];
-        callback.apply(Voltron, args);
-      });
-      return this;
-    },
-
-    debug: function(){
-      if(this.isDebugging()){
-        var method = arguments[0];
-        var args = Array.prototype.slice.call(arguments, 1);
-        console[method].apply(this, args);
-      }
-      return this;
-    },
-
-    // Get a config value, optionally define a default value in the event the config param is not defined
-    getConfig: function(key, def){
-      if(!key) return config;
-      var paths = key.replace(/(^\/+)|(\/+$)/g, '').split('/');
-      var out = config;
-
-      $.each(paths, function(index, path){
-        if(out[path] != undefined){
-          out = out[path];
-        }else{
-          out = def;
-          return false;
-        }
-      });
-
-      return out;
-    },
-
-    // Set a config value
-    setConfig: function(key, value){
-      config[key] = value;
-      return this;
-    },
-
-    // Similar to setConfig, except this will instead treat the config `key` value as an array, and add the value to it
-    addConfig: function(key, value){
-      if(!config[key]) config[key] = [];
-      config[key].push(value);
-      return this;
-    },
-
-    getAuthToken: function(){
-      return this.getConfig('auth_token', '');
-    },
-
-    // Are we in debug mode?
-    isDebugging: function(){
-      return this.getConfig('debug', false);
-    },
-
-    isController: function(controllers){
-      return [controllers].flatten().compact().includes(this.getConfig('controller'));
-    },
-
-    // Adds one or more event listener callbacks that will be dispatched when the event occurs
-    // Example: Voltron.on('event1', 'event2', 'event3', function(observer){});
-    on: function(){
-      var events = Array.prototype.slice.call(arguments, 0, -1);
-      var callback = Array.prototype.slice.call(arguments, -1).pop();
-      $.each(events, function(index, event){
-        if(!observer[event]) observer[event] = [];
-        observer[event].push(callback);
-      });
-      return this;
-    },
-
-    // Dispatch an event, optionally providing some additional params to pass to the event listener callback
-    dispatch: function(name, params){
-      if(!params) params = {};
-      this.debug('info', 'Dispatching %o', name);
-      $.each(observer[name], function(index, callback){
-        callback.call(Voltron, params);
-      });
-      return this;
-    },
-
-    new: function(id){
-      if(this.hasModule(id)){
-        if($.isFunction(this.getModule(id).new)){
-          return this.getModule(id).new.apply(this.getModule(id), Array.prototype.slice.call(arguments, 1));
-        }else{
-          this.debug('warn', 'Module %o has not defined a %o method.', id, 'new');
-        }
-      }else{
-        this.debug('warn', 'Module with name %o does not exist.', id);
-      }
-      return false;
-    },
-
-    // Check if a module with the given name has been added
-    hasModule: function(id){
-      return modules[id.toLowerCase()] != undefined;
-    },
-
-    // Add a module, specifying the name (id), the module itself (should be an object or a function that returns such)
-    // Optionally provide `true`, or an array of controller names as the last argument to auto instantiate when added either
-    // all the time (if true), or on the specified controllers
-    addModule: function(){
-      var id = arguments[0];
-      var depends = typeof arguments[1] == 'function' ? [] : arguments[1];
-      var module = typeof arguments[1] == 'function' ? arguments[1] : arguments[2];
-      var run = typeof arguments[1] == 'function' ? arguments[2] : arguments[3];
-
-      if(!this.hasModule(id)){
-        modules[id.toLowerCase()] = module;
-      }
-
-      // Wait until DOM loaded, then create instances of any modules that should be created
-      this.ready(function(id, depends, run){
-        if(run === true || (this.isController(run) && run !== false)){
-          for(var i=0; i<depends.length; i++){
-            this.getModule(depends[i]);
-          }
-          this.getModule(id);
-        }
-      }, [id, depends, run]);
-      return this;
-    },
-
-    // Get a module with the given name from the list of modules
-    getModule: function(name, args){
-      var id = name.toLowerCase();
-      if(!args) args = [];
-      if(this.hasModule(id)){
-        if(!classes[id]){
-          classes[id] = new modules[id]($);
-          this.debug('info', 'Instantiated %o', name);
-          if(classes[id].initialize){
-            Voltron.dispatch('before:module:initialize:' + id, { module: classes[id] });
-            classes[id].initialize.apply(classes[id], args);
-            Voltron.dispatch('after:module:initialize:' + id, { module: classes[id] });
-          }
-        }
-        return classes[id];
-      }else{
-        this.debug('warn', 'Module with name %o does not exist.', name);
-      }
-      return false;
+  if(Voltron.hasModule(module)){
+    var mod = Voltron.getModule(module);
+    if($.isFunction(mod[method])){
+      return mod[method].apply(mod, Array.prototype.slice.call(arguments, 1));
+    }else{
+      Voltron.debug('error', 'Module %o does not define the method %o', module, method);
     }
-  };
-})(jQuery);
+  }else{
+    Voltron.debug('error', 'Module with name %o does not exist.', module);
+  }
+  return false;
+});
+
+$.extend(Voltron, {
+  _config: {},
+  _observer: {},
+  _modules: {},
+  _classes: {},
+
+  _inherited: {
+    on: function(){
+      var args = Array.prototype.slice.call(arguments, 0);
+      args.push(this);
+      Voltron.on.apply(Voltron, args);
+      return this;
+    }
+  },
+
+  initialize: function(conf){
+    if(!conf) conf = {};
+    $.extend(this._config, conf);
+
+    // Try and create a module with the name of the current controller
+    if(this.hasModule(this.getConfig('controller'))){
+      this.ready(Voltron.getModule, this.getConfig('controller'));
+    }
+  },
+
+  // When ready, fire the callback function, passing in any additional args
+  ready: function(callback, args){
+    $(document).ready(function(){
+      if(!$.isArray(args)) args = [args];
+      callback.apply(Voltron, args);
+    });
+    return this;
+  },
+
+  debug: function(){
+    if(this.isDebugging()){
+      var method = arguments[0];
+      var args = Array.prototype.slice.call(arguments, 1);
+      console[method].apply(this, args);
+    }
+    return this;
+  },
+
+  // Get a config value, optionally define a default value in the event the config param is not defined
+  getConfig: function(key, def){
+    var out = this._config;
+    if(!key) return out;
+    var paths = key.replace(/(^\/+)|(\/+$)/g, '').split('/');
+
+    $.each(paths, function(index, path){
+      if(out[path] != undefined){
+        out = out[path];
+      }else{
+        out = def;
+        return false;
+      }
+    });
+
+    return out;
+  },
+
+  // Set a config value
+  setConfig: function(key, value){
+    this._config[key] = value;
+    return this;
+  },
+
+  // Similar to setConfig, except this will instead treat the config `key` value as an array, and add the value to it
+  addConfig: function(key, value){
+    if(!this._config[key]) this._config[key] = [];
+    this._config[key].push(value);
+    return this;
+  },
+
+  getAuthToken: function(){
+    return this.getConfig('auth_token', '');
+  },
+
+  // Are we in debug mode?
+  isDebugging: function(){
+    return this.getConfig('debug', false);
+  },
+
+  isController: function(controllers){
+    return [controllers].flatten().compact().includes(this.getConfig('controller'));
+  },
+
+  // Adds one or more event listener callbacks that will be dispatched when the event occurs
+  // Optionally with a defined context for what `this` will be in the callback function
+  // If not defined it defaults to the core Voltron module, aka - the stuff in this file
+  // Example: Voltron.on('event1', 'event2', 'event3', function(observer){}, this);
+  on: function(){
+    var args = Array.prototype.slice.call(arguments, 0);
+    var events = $.map(args, function(item){ if(typeof item == 'string') return item; });
+    var callback = args[events.length];
+    var context = args[events.length+1] || Voltron;
+
+    $.each(events, function(index, event){
+      if(!Voltron._observer[event]) Voltron._observer[event] = [];
+      Voltron._observer[event].push($.proxy(callback, context));
+    });
+    return this;
+  },
+
+  // Dispatch an event, optionally providing some additional params to pass to the event listener callback
+  dispatch: function(name, params){
+    if(!params) params = {};
+    this.debug('info', 'Dispatching %o', name);
+    $.each(this._observer[name], function(index, callback){
+      callback.call(Voltron, params);
+    });
+    return this;
+  },
+
+  // Check if a module with the given name has been added
+  hasModule: function(id){
+    return this._modules[id.toLowerCase()] != undefined;
+  },
+
+  // Add a module, specifying the name (id), the module itself (should be an object or a function that returns such)
+  // Optionally provide `true`, or an array of controller names as the last argument to auto instantiate when added either
+  // all the time (if true), or on the specified controllers
+  addModule: function(){
+    var id = arguments[0];
+    var depends = $.isFunction(arguments[1]) ? [] : arguments[1];
+    var module = $.isFunction(arguments[1]) ? arguments[1] : arguments[2];
+    var run = $.isFunction(arguments[1]) ? arguments[2] : arguments[3];
+
+    if(!this.hasModule(id)){
+      this._modules[id.toLowerCase()] = module;
+    }
+
+    // Wait until DOM loaded, then create instances of any modules that should be created
+    this.ready(function(id, depends, run){
+      if(run === true || (this.isController(run) && run !== false)){
+        for(var i=0; i<depends.length; i++){
+          this.getModule(depends[i]);
+        }
+        this.getModule(id);
+      }
+    }, [id, depends, run]);
+    return this;
+  },
+
+  // Get a module with the given name from the list of modules
+  getModule: function(name, args){
+    var id = name.toLowerCase();
+    if(!args) args = [];
+    if(this.hasModule(id)){
+      if(!this._classes[id]){
+        this._classes[id] = new this._modules[id]($);
+        // Add some inherited methods... shortcuts, if you will
+        this._classes[id] = $.extend(this._classes[id], this._inherited);
+        // Tell the user we've created the module
+        this.debug('info', 'Instantiated %o', name);
+        // If there is an initialize function, call it, dispatching before/after events
+        if($.isFunction(this._classes[id].initialize)){
+          Voltron.dispatch('before:module:initialize:' + id, { module: this._classes[id] });
+          this._classes[id].initialize.apply(this._classes[id], args);
+          Voltron.dispatch('after:module:initialize:' + id, { module: this._classes[id] });
+        }
+      }
+      return this._classes[id];
+    }else{
+      this.debug('warn', 'Module with name %o does not exist.', name);
+    }
+    return false;
+  }
+});
+
+if(typeof vv != 'undefined'){
+  console.warn('The window variable %o is already defined, so shortcut to %o will not be defined.', 'vv', 'Voltron');
+}else{
+  window.vv = window.Voltron;
+}
--- app/assets/javascripts/voltron-dispatch.js
+++ app/assets/javascripts/voltron-dispatch.js
@@ -1,4 +1,5 @@
-//= require voltron
+//= require voltron-ext
+//= require voltron-core
 
 Voltron.addModule('Dispatch', function(){
   var events = {};
@@ -9,7 +10,7 @@ Voltron.addModule('Dispatch', function(){
       events[event] = args;
       $.each(args, function(index, evt){
         if(['element', 'event', 'data'].includes(evt.toLowerCase())){
-          Voltron.debug('error', "Provided event watcher argument '" + evt + "' is a reserved observer param and will be overridden when the event is dispatched. Consider changing the name of the argument in your call to addEventWatcher.");
+          Voltron.debug('error', "Provided event watcher argument %o is a reserved observer param and will be overridden when the event is dispatched. Consider changing the name of the argument in your call to addEventWatcher for %o", evt, event);
         }
       });
       Voltron.debug('info', 'Added event watcher for %o', event);
@@ -52,13 +53,15 @@ Voltron.addModule('Dispatch', function(){
       args = Voltron.getModule('Dispatch').getArgumentHash(event.type, args);
       var params = $.extend(args, { element: this, event: event, data: $(this).data() });
 
-      var events = $(this).data('dispatch').toString().split(/\s+/);
+      var events = params.data.dispatch.split(/\s+/);
 
       if(events.includes(event.type)){
-        if($(this).data('event')){
-          Voltron.dispatch([event.type, this.tagName, $(this).data('event')].compact().join(':').toLowerCase(), params);
+        if(params.data.event){
+          Voltron.dispatch([event.type, params.data.event].join(':').toLowerCase(), params);
+        }else if(this.id){
+          Voltron.dispatch([event.type, this.id].join(':').toLowerCase(), params);
         }else{
-          Voltron.dispatch([event.type, this.tagName, this.id].compact().join(':').toLowerCase(), params);
+          Voltron.dispatch([event.type, this.tagName].join(':').toLowerCase(), params);
         }
       }
     }
