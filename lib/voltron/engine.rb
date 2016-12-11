5ea3e399840931e5285b9120b4e1f15ba673d569
Eric Hainer <eric.hainer@gmail.com>
2016-12-10 22:08:58 -0700

    Include voltron helper via engine

--- lib/voltron/engine.rb
+++ lib/voltron/engine.rb
@@ -5,5 +5,9 @@ module Voltron
 
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
+    initializer "voltron.initialize" do
+      ::ActionController::Base.send :helper, ::VoltronHelper
+    end
+
   end
 end
--- spec/railsapp/app/controllers/home_controller.rb
+++ spec/railsapp/app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    flash! notice: "Test"
     headers["Cache-Control"]  = "no-cache"
     headers.delete "Last-Modified"
     headers.delete "ETag"
