9b124bd5d472065c8439e9fdbdb02e995fd34c40
Eric Hainer <eric.hainer@gmail.com>
2016-12-11 22:16:13 -0700

    Adds tests for config merge method, removes flash tests (separate gem)

--- spec/voltron/config_spec.rb
+++ spec/voltron/config_spec.rb
@@ -8,4 +8,10 @@ describe Voltron::Config do
     expect(config.js).to be_a(Voltron::Config::Js)
   end
 
+  it "can merge in additional config" do
+    config.merge({ dummy: "data" })
+    expect(config.to_h).to have_key(:dummy)
+    expect(config.to_h[:dummy]).to eq("data")
+  end
+
 end
